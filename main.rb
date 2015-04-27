200216ab48ac8e5d99173e0ca4f388c89d76914c
rezerbit <rezerbit@gmail.com>
2015-04-27 12:57:01 +0500

    parse xlsx

--- main.rb
+++ main.rb
@@ -1,4 +1,11 @@
 require 'roo'
 
 xlsx = Roo::Spreadsheet.open('menu.xlsx', extension: :xlsx)
-p xlsx.info
+sheet = xlsx.sheet(0)
+
+rows = []
+sheet.each(name: 'Элемент меню', controller: 'Controller', action: 'Action' ) do |hash|
+  rows << hash
+end
+
+rows
